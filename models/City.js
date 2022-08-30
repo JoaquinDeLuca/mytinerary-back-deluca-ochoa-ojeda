ee16e4e48f97e3e0f596f3355670f330b49ba73d
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-30 02:47:17 -0300

    nuevo modelo

---
+++ models/City.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+// Aca defino el tabla o mejor dicho el esquema de datos
+const schema = new mongoose.Schema({
+    city: {type: String, required: true},
+    country: {type: String, required: true},
+    photo: {type: String, required: true},
+    population: {type: Number, required: true},
+    fundation: {type: Date, required: true},
+
+})
+
+// Aca defino el modelo de coleccion con el nombre cities
+const City = mongoose.model(
+    'cities',
+    schema
+    // Nombre de la coleccion 
+    // Esquema de datos 
+)
+
+// Aca exporto el modelo
+module.exports = City
