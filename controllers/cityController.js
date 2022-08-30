9bcdf2a3f55b82180eac37946ffb732f24f4be38
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-30 20:26:15 -0300

    se agrega un nuevo controlador

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -17,6 +17,31 @@ const cityContoller = {
 
 
         }
+    },
+    read: async(req, res) => {
+        const {id} = req.params
+        try{
+            let city = await City.findOne({_id:id})
+
+            if (city) {
+                res.status(200).json({
+                    message: 'you get one city',
+                    response: city,
+                    success: true
+                })
+            }else{
+                res.status(404).json({
+                    message: "could't find city",
+                    success: false
+                })
+            }
+
+        }catch(error){
+            res.status(400).json({
+                message: "error",
+                success: false
+            })
+        }
     }
 }
 
