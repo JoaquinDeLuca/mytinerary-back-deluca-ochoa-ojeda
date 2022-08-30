4a8571addc58ba4d792bf8b1598e66fef8821853
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-30 20:27:07 -0300

    se agrega la ruta del controlador

--- routes/cities.js
+++ routes/cities.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 // importando y saco la propiedad create de ese objeto
-const {create} = require('../controllers/cityController') 
+const {create,read} = require('../controllers/cityController') 
 
 
 
 // router.metodo('la ruta', controlador)
 
 router.post('/',create)
+router.get('/:id',read)
 
 module.exports = router;
