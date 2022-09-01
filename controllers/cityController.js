490ee06479c8fbd841d14a3ef68494324ca1e5b5
LilAguh <aguezeochoa@gmail.com>
2022-08-31 22:53:36 -0300

    se agrega el filtro find start a controller

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,15 +1,15 @@
 const City = require('../models/City')
 
 const cityContoller = {
-    create: async(req,res) => {
-        const {city,country,photo,population,fundation} = req.body
+    create: async (req, res) => {
+        const { city, country, photo, population, fundation } = req.body
         try {
             await new City(req.body).save() // req.body tiene que tener, todas las varibles descriptas
             res.status(201).json({
                 message: 'city created',
                 success: true
             })
-        } catch(error){
+        } catch (error) {
             res.status(400).json({
                 message: "could't create city",
                 success: false
@@ -18,10 +18,10 @@ const cityContoller = {
 
         }
     },
-    read: async(req, res) => {
-        const {id} = req.params
-        try{
-            let city = await City.findOne({_id:id})
+    read: async (req, res) => {
+        const { id } = req.params
+        try {
+            let city = await City.findOne({ _id: id })
 
             if (city) {
                 res.status(200).json({
@@ -29,14 +29,14 @@ const cityContoller = {
                     response: city,
                     success: true
                 })
-            }else{
+            } else {
                 res.status(404).json({
                     message: "could't find city",
                     success: false
                 })
             }
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
             res.status(400).json({
                 message: "error",
@@ -44,19 +44,25 @@ const cityContoller = {
             })
         }
     },
-    all: async (req,res)=>{
+    all: async (req, res) => {
         let cities
-        try{
-            cities = await City.find()
+        let query = {}
+        let startWidth = { $regex: "^" + req.query.city, $options: 'i' + req.query.city }
+
+        if (req.query.city) {
+            query.city = req.query.city
+        }
+        try {
+            cities = await City.find({ city: startWidth })
             res.json(cities)
-        } catch(error){
+        } catch (error) {
             console.log(error)
             res.status(500).json({
                 message: "server error",
                 success: false
             })
         }
-        
+
     }
 
 }
