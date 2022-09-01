f89be483a501f0ec9b496925f03d988f2207e090
LilAguh <aguezeochoa@gmail.com>
2022-08-31 23:11:48 -0300

    se agregan las rutas del controlador de update y remove

--- routes/cities.js
+++ routes/cities.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // importando y saco la propiedad create de ese objeto
-const {all,create,read} = require('../controllers/cityController') 
+const { all, create, read, remove, update } = require('../controllers/cityController')
 
 
 
@@ -10,8 +10,10 @@ const {all,create,read} = require('../controllers/cityController')
 
 
 router.get('/', all)
-router.get('/:id',read)
-router.post('/',create)
+router.get('/:id', read)
+router.post('/', create)
+router.delete('/:id', remove)
+router.put('/:id', update)
 
 //exporta el enrutador
 module.exports = router;
