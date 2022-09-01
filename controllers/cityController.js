c32f3e16f67cac7b06c5e4ac257399234d521075
LilAguh <aguezeochoa@gmail.com>
2022-08-31 23:07:52 -0300

    se agrega metodo de delete y update en cityController

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -63,8 +63,62 @@ const cityContoller = {
             })
         }
 
-    }
+    },
+    //funcion que elimina ciudades por la id
+    remove: async (req, res) => {
+        const { id } = req.params
 
+        try {
+            let city = await City.deleteOne({ _id: id })
+            if (city) {
+                res.status(200).json({
+                    message: "the city was successfully deleted",
+                    success: true
+                })
+            }
+            else {
+                res.status(404).json({
+                    message: "city not found",
+                    success: false
+                })
+            }
+        }
+        catch (error) {
+            console.log(error)
+            res.status(400).json({
+                message: "error in delete this city",
+                success: false
+            })
+        }
+    },
+    //function que modifica las ciudades exitentes
+    update: async (req, res) => {
+        const { id } = req.params
+        const makeChanges = req.body
+
+        try {
+            let city = await City.updateOne({ _id: id }, makeChanges, { new: true })
+            if (city) {
+                res.status(201).json({
+                    message: "the city was successfully modified",
+                    success: true
+                })
+            }
+            else {
+                res.status(404).json({
+                    message: "city not found",
+                    success: false
+                })
+            }
+        }
+        catch (error) {
+            console.log(error)
+            res.status(400).json({
+                message: "error modifying this city",
+                success: false
+            })
+        }
+    }
 }
 
 module.exports = cityContoller
