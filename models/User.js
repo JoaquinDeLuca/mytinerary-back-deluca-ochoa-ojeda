71f920ac5232ea7064b2cf5320813b4aa85dbc8e
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-02 18:24:32 -0300

    Se creo el modelo para User

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema ({
+
+    name:{ type: String, required: true },
+    lastName: { type: String, required: true },
+    mail: { type: String, required: true },
+    password:{ type: String, required: true},
+    photo:{ type: String, required: true },
+    country:{ type: String, required: true },
+
+})
+
+const User = mongoose.model(
+    'Users',
+    schema
+)
+
+module.exports = User
