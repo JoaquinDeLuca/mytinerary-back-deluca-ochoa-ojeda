b538e7171be23043d987497fbd0ae0d786366e24
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-02 20:44:12 -0300

    se crea modelo de Itinerary

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
 
-    name:{     type: String, required: true },
-    user:{     type: String },
-    city:{     type: String},
-    price:{    type: Number, required: true},
-    likes:{    type: Array, required: true},
-    tags:{     type: Array, required: true},
-    duration:{ type: Number, required: true },
+    name:{      type: String, required: true },
+    user:{      type: String },
+    city:{      type: String},
+    price:{     type: Number, integer: true , required: true},
+    likes:[{    type: String, required: true}],
+    tags:[{     type: String, required: true}],
+    duration:{  type: Number, integer: true, required: true },
 })
 
 const Itinerary = mongoose.model(
-    'itinerarys',
+    'itineraries',
     schema
 )
 
