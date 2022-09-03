71f60d7ee7e5ce469f13f9dac56192de3978bd02
LilAguh <aguezeochoa@gmail.com>
2022-09-03 04:53:15 -0300

    se agrega la ruta de delete en itineraries

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
-const { create, patch } = require('../controllers/itineraryController')
+const { create, patch, remove } = require('../controllers/itineraryController')
 
 router.post('/', create)
 router.put('/:id', patch)
+router.delete('/:id', remove)
 
 module.exports = router
