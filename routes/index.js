08f3d954b6edd2c7abb762716a135588fa5090c1
LilAguh <aguezeochoa@gmail.com>
2022-09-03 04:20:50 -0300

    se agrega itinerary a las rutas de index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const cityRouter = require('./cities')
 const userRouter = require('./users')
+const itineraryRouter = require('./itineraries')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,5 +14,6 @@ router.get('/', function (req, res, next) {
 // Va a unir todas las rutas en index
 router.use('/cities', cityRouter)
 router.use('/users', userRouter)
+router.use('/itineraries', itineraryRouter)
 
 module.exports = router;
