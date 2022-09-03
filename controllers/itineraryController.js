23b9248fa65fc2cf987e4aa9c5655424d066b537
LilAguh <aguezeochoa@gmail.com>
2022-09-03 04:45:33 -0300

    se agrega patch en el controlador de itinerarios

--- controllers/itineraryController.js
+++ controllers/itineraryController.js
@@ -15,6 +15,34 @@ const itineraryController = {
                 success: false
             })
         }
+    },
+    patch: async (req, res) => {
+        const { id } = req.params
+        const makeChanges = req.body
+
+        try {
+            let itinerary = await Itinerary.updateOne({ _id: id }, makeChanges, { new: true })
+
+            if (itinerary) {
+                res.status(201).json({
+                    message: "itinerary was successfully modified",
+                    success: true
+                })
+            }
+            else {
+                res.status(404).json({
+                    message: "itinerary not found",
+                    success: false
+                })
+            }
+        }
+        catch (error) {
+            console.log(error)
+            res.status(400).json({
+                message: "error modifying this itinerary",
+                success: false
+            })
+        }
     }
 }
 
