f6f1960e2a6681f22d13720a992bece8e32eae46
LilAguh <aguezeochoa@gmail.com>
2022-09-03 02:57:47 -0300

    se une la ruta de user en index

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ const cityRouter = require('./cities')
 const userRouter = require('./users')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // res.json();
-  res.render('index', {title: 'MyTinerary'});
+  res.render('index', { title: 'MyTinerary' });
 });
 
 
 // Va a unir todas las rutas en index
-router.use('/cities',cityRouter)
-router.use('/user', userRouter)
+router.use('/cities', cityRouter)
+router.use('/users', userRouter)
 
 module.exports = router;
