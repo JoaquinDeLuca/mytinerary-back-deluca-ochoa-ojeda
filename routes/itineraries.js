ee1ef0e47306ac96fe88b0a374c8507d3d9c3cb7
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-07 06:31:13 -0300

    ruta para itinerarios de un usuario

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var router = express.Router()
 
-const { create, patch, remove, read, readFromCity } = require('../controllers/itineraryController')
+const { create, patch, remove, read, readFromCity , readFromUser} = require('../controllers/itineraryController')
 
 router.get('/', read) // read all itineraries 
+router.get('/user/:id', readFromUser)
 router.get('/city/:id', readFromCity) // read from city id 
 router.post('/', create)
 router.put('/:id', patch)
