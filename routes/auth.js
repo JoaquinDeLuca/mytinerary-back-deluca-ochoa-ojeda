99e77291d45fef5556057e8ef047b173586939a3
LilAguh <aguezeochoa@gmail.com>
2022-09-09 19:35:35 -0300

    la ruta users, pasa a ser auth

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const { signUp } = require('../controllers/userController')
+
+
+router.post('/signup', signUp)
+
+module.exports = router;
