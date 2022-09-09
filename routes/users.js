e538d5b00708b1cf48dac9df00943531dc23cf19
LilAguh <aguezeochoa@gmail.com>
2022-09-09 19:34:01 -0300

    se cambia el nombre de la ruta a auth

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const { create } = require('../controllers/userController')
-
-
-router.post('/', create)
-
-module.exports = router;
