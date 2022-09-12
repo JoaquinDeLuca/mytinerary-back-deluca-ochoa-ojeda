e91db2a956aec50eaefe3b2849e09a74ebcf02a7
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-12 11:41:47 -0300

    se agrego la ruta para el nuevo controlador

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const { signUp } = require('../controllers/userController')
+const { signUp, verifyMail } = require('../controllers/userController')
 
 
 router.post('/signup', signUp)
+router.get('/verify/:code', verifyMail)
 
 module.exports = router;
