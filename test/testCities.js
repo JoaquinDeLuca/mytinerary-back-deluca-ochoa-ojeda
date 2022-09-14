0f3b9dd3dbaab4d01b4bf078b9950ba508a19851
LilAguh <aguezeochoa@gmail.com>
2022-09-14 05:58:26 -0300

    se agrega nuevo test para usuarios

--- test/testCities.js
+++ test/testCities.js
@@ -4,44 +4,44 @@ const app = require('../app')
 
 describe('POST /cities', function () {
 
-    it('Must respond with the id',function(done){
+    it('Must respond with the id', function (done) {
         request(app)
             .post('/cities')
             .send({
-                city:"Test", 
-                country:"spain", 
-                photo:"http://localHost:400/images", 
-                population:"16433", 
-                fundation:"1550", 
-                information:" es muy lindo "
+                city: "Test",
+                country: "spain",
+                photo: "http://localHost:400/images",
+                population: "16433",
+                fundation: "1550",
+                information: " es muy lindo "
             })
-            .then( response => {
+            .then(response => {
                 assert.isString(response.body.id)
                 done()
             })
     })
 
-    it('Must respond with 201 status code', function (done){
+    it('Must respond with 201 status code', function (done) {
         request(app)
             .post('/cities')
             .send({
-                city:"Test", 
-                country:"spain", 
-                photo:"http://localHost:400/images", 
-                population:"16433", 
-                fundation:"1550", 
-                information:" es muy lindo "
+                city: "Test",
+                country: "spain",
+                photo: "http://localHost:400/images",
+                population: "16433",
+                fundation: "1550",
+                information: " es muy lindo "
             })
             .expect(201, done)
     })
 
-    it('Must respond with 400 status code', function (done){
+    it('Must respond with 400 status code', function (done) {
         request(app)
             .post('/cities')
             .send({})
             .expect(400)
-            .end(function (err, res){
-                if(err) return done(err)
+            .end(function (err, res) {
+                if (err) return done(err)
                 return done()
             })
     })
