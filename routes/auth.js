02823b0bf3dcc2c73374124ea7fc4d71df6f4699
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-15 21:46:06 -0300

    ruta para buscar usuario por id

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const { signIn, signUp, verifyMail, signOut } = require('../controllers/userController')
+const { signIn, signUp, verifyMail, signOut, readUser } = require('../controllers/userController')
 
 
 router.post('/signup', signUp)
 router.post('/signin', signIn)
+router.get('/:id',readUser)
 router.get('/verify/:code', verifyMail)
 router.put('/signout/:mail', signOut)
 
