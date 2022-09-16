55507b7967beffee20f9102fd269fd812b6674eb
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-15 21:46:46 -0300

    ruta para bucas itinerario por id

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
-const { create, patch, remove, read, readFromCity , readFromUser} = require('../controllers/itineraryController')
+const { create, patch, remove, read, readFromCity , readFromUser, readFromId} = require('../controllers/itineraryController')
 
 router.get('/', read) // read all itineraries 
 router.get('/user/:id', readFromUser)
-router.get('/city/:id', readFromCity) // read from city id 
+router.get('/city/:id', readFromCity) // read from city id
+router.get('/:id',readFromId)
 router.post('/', create)
 router.put('/:id', patch)
 router.delete('/:id', remove)
