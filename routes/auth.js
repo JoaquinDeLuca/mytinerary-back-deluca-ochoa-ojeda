a6085f392a00e1454b5c42ec69dc9c18c94e85be
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-20 17:52:49 -0300

    ruta para el token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let passport = require('../config/passport')
 
 const { signIn, signUp, verifyMail, signOut, readUser } = require('../controllers/userController')
 
@@ -9,5 +10,7 @@ router.post('/signin', signIn)
 router.get('/:id',readUser)
 router.get('/verify/:code', verifyMail)
 router.put('/signout/:mail', signOut)
+
+router.get('/token', passport.authenticate('jwt', {session:false}) , 'elmetodo ej:verifytoken' )
 
 module.exports = router;
