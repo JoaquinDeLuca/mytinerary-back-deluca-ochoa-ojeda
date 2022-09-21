66fffd190676cd3b5575894bdb0f73796c6d7f26
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-21 17:11:47 -0300

    se agrego la ruta para likeAndDislike y passport

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,7 +1,16 @@
 var express = require('express')
 var router = express.Router()
+let passport = require('../config/passport')
 
-const { create, patch, remove, read, readFromCity , readFromUser, readFromId} = require('../controllers/itineraryController')
+const { 
+    create, 
+    patch, 
+    remove, 
+    read, 
+    readFromCity , 
+    readFromUser, 
+    readFromId , 
+    likeAndDislike} = require('../controllers/itineraryController')
 
 router.get('/', read) // read all itineraries 
 router.get('/user/:id', readFromUser)
@@ -10,5 +19,7 @@ router.get('/:id',readFromId)
 router.post('/', create)
 router.put('/:id', patch)
 router.delete('/:id', remove)
+router.patch('/like/:id', passport.authenticate('jwt', {session:false}) ,likeAndDislike)
+    
 
 module.exports = router
